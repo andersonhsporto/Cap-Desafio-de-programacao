9cdfbc8fc29991a59fafe3085ddfbe2db7a00ccb
Anderson Porto <andersonhigo2@gmail.com>
2022-03-03 17:11:02 -0300

    build: desafio 2

--- Desafio-2/README.md
+++ Desafio-2/README.md
@@ -1,5 +1,7 @@
 #  Desafio de programação 02
 
+# Questão 01
+
 Desenvolva um algoritmo que encontre a mediana.
 
 ## Exemplo:
@@ -17,11 +19,13 @@ Desenvolva um algoritmo que encontre a mediana.
 #### Utilização
 
 Após compilar utilizando make execute o binário questao_1 da seguinte forma:
+O parametro é vetor de inteiros. 
 ```
 	./questao_1 9 2 1 4 6
 ```
 
 <hr>
+
 # Questão 02
 
 Dado um vetor de inteiros n e um inteiro qualquer x. Construa um algoritmo que determine o número de elementos pares do vetor que tem uma diferença igual ao valor de x;
@@ -42,36 +46,46 @@ Dado um vetor de inteiros n e um inteiro qualquer x. Construa um algoritmo que d
 #### Utilização
 
 Após compilar utilizando make execute o binário questao_2 da seguinte forma:
-O parametros são numero x seguido do vetor de inteiros. 
+O parametro é o numero x seguido do vetor de inteiros. 
 ```
 	./questao_2 3 1 5 3 4 2
 ```
 
 
 <hr>
+
 # Questão 03
 
 Um texto precisa ser encriptado usando o seguinte esquema. Primeiro, os espaços são removidos do texto. Então, os caracteres são escritos em um grid, no qual as linhas e colunas tem as seguintes regras:
 
-    - Considere T, como o tamanho do texto.
-    - Se certifique de que linhas x colunas >= .
-    - Se múltiplos grids satisfazem as condições, escolha aquele com a menor área.
+- Considere T, como o tamanho do texto.
+- Se certifique de que linhas x colunas >= .
+- Se múltiplos grids satisfazem as condições, escolha aquele com a menor área.
 
 Escreva um algoritmo que ao receber uma string s, mostre a mensagem encriptada de acordo com as regras descritas.
 ## Exemplo:
 
 #### Entrada
 ```C
-s = tenha um bom dia
+    s = tenha um bom dia
 ```
 
 #### Saída
 ```C
-taoa eum nmd hbi
+    taoa eum nmd hbi
+```
+
+#### Utilização
+
+Após compilar utilizando make execute o binário questao_3 da seguinte forma:
+O parametro é a frase a ser encriptada
+```
+	./questao_3 "tenha um bom dia" 
 ```
 
 
 
+# Compilação
 Para compilar os 3 exercicios utilize o comando `make`
 ```
 Para compilar somente o primeiro exercício  utilize o comando: make 1
--- Desafio-2/q3.c
+++ Desafio-2/q3.c
@@ -59,41 +59,134 @@ char	*remove_spaces(char **string)
 	return (tmp);
 }
 
-char	*matrix(char **argv)
+static size_t	check(const char *s, unsigned int start, size_t len)
+{
+	size_t	s_len;
+	size_t	size;
+
+	s_len = strlen(s);
+	size = 0;
+	if (start > s_len)
+	{
+		size = 1;
+	}
+	else if (start + len > s_len)
+	{
+		size = s_len - start + 1;
+	}
+	else
+	{
+		size = len + 1;
+	}
+	return (size);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*substr;
+
+	if (s == 0)
+		return (0);
+	substr = malloc((check(s, start, len)) * sizeof(char));
+	i = 0;
+	if (substr == 0)
+		return (0);
+	while ((len--) && (s[start + i] != '\0') && ((start + i) < strlen(s)))
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	**start_matrix(char **argv)
 {
 	char	**matrix;
-	char	*string = remove_spaces(argv);
-	size_t	len = strlen(string);
-	size_t	size = my_sqrt(len);
-	matrix = malloc((len / size) * sizeof(char *));
-	int i, j, k;
 
+	char	*string = remove_spaces(argv);
+	size_t	size = my_sqrt(strlen(string));
+	matrix = malloc((strlen(string) / size) * sizeof(char *));
+	int i, j;
+
+	i = 0;
 	j = 0;
-	k = 0;
-	while ((string[j] != '\0'))
+	while (i < size + 1)
 	{
-		matrix[k] = (char *)malloc(sizeof(char *) * size + 1);
-		while (k < size)
+		matrix[i] = ft_substr(string, j, size + 1);
+		j +=size + 1;
+		i++;
+	}
+	free(string);
+	string = NULL;
+	return (matrix);
+}
+
+int	matrix_len(char **matrix)
+{
+	int i = 0;
+	while (matrix[i])
+	{
+		i++;
+	}
+	return(i);
+}
+
+char	replace_string(char *string, int size, int len)
+{
+	int		i = 0;
+	int		j = 0;
+	char	*tmp;
+
+
+	while (i < size)
+	{
+		if (string[i] == '\0')
+			string[i] = ' ';
+		i++;
+	}
+	if (string[len] != ' ')
+	{
+		i = size;
+		while (i > len)
+		{
+			string[i] = string[i - 1];
+			i--;
+		}
+		string[len] = ' ';
+	}
+}
+
+
+char	*encript(char **argv)
+{
+	char	**matrix = start_matrix(argv);
+	char	*string = calloc(sizeof(char), strlen(argv[1]));
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	while (j < strlen(argv[1]))
+	{
+		string[j] = matrix[i][k];
+		i++;
+		j++;
+		if (i == matrix_len(matrix))
 		{
 			i = 0;
-			while (i < size)
-			{
-				matrix[k][i] = string[j];
-				i++;
-			}
-			matrix[k][i] = '\0';
 			k++;
 		}
-		j++;
 	}
-	return (NULL);
+	replace_string(string, strlen(argv[1]), matrix_len(matrix));
+	return (string);
 }
 
 
 int		main(int argc, char **argv)
 {
-	char *teste = matrix(argv);
+	char	*cript_string = encript(argv);
 
-	free(teste);
+	printf("%s\n", cript_string);
+	free(cript_string);
 	return (0);
 }
